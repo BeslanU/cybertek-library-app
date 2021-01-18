9a491981332742f0f1389f638768d4950e4b76a1
BeslanU <92beslan@gmail.com>
2021-01-17 20:39:56 -0600

    Second one

--- src/test/java/com/cybertek/library/runners/CukesRunner.java
+++ src/test/java/com/cybertek/library/runners/CukesRunner.java
@@ -7,9 +7,12 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = {
+                "json:target/cucumber.json"
+        },
         features = "src/test/resources/features",
         glue = "com/cybertek/library/step_definitions",
-        tags = "@all_accounts"
+        tags = "@wip"
 )
 public class CukesRunner {
 }
